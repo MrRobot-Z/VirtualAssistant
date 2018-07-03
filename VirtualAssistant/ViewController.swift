ba1084cae83f3f914feba723d655012537c90364
Moustafa Zaher <m.zaher2030@yahoo.com>
2018-07-03 13:30:19 +0200

    Add Wit Module

--- VirtualAssistant/ViewController.swift
+++ VirtualAssistant/ViewController.swift
@@ -7,16 +7,21 @@
 //
 
 import UIKit
+import SwiftyJSON
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,WitDelegate{
     
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var label: UILabel!
+    
+    let wit = WitModule()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        wit.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,10 +30,21 @@ class ViewController: UIViewController {
     }
 
     @IBAction func goButtonPressed(_ sender: UIButton) {
-        let text = textField.text
+        var text = textField.text
         textField.text = ""
         
-        print(text!)
+        text = "صحينى بكره الساعة خامسة"
+        
+        wit.witNetwork(toBeSentString: text!)
+        
+    }
+    
+    func recieveResponse(response: JSON) {
+        print("Here***********************************")
+        print(response.rawString()!)
+        print(response.count)
+        print(response["entities"]["intent"][0]["value"])
+        print(response["entities"]["intent"][0]["confidence"])
     }
     
 }
---
+++ VirtualAssistant/WitModule.swift
@@ -0,0 +1,50 @@
+//
+//  WitModule.swift
+//  VirtualAssistant
+//
+//  Created by MZaher on 7/3/18.
+//  Copyright © 2018 MZaher. All rights reserved.
+//
+
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+protocol WitDelegate {
+    func recieveResponse(response: JSON) -> Void
+}
+
+class WitModule{
+    
+    var delegate: WitDelegate?
+    
+    let baseURL = "https://api.wit.ai/message"
+    //let witAppToken = " Bearer EDM7POFMZLZ6H2OB253HNBAVYPBKW2RC"
+    let witAppToken = " Bearer AAP6LXNLDDYAAPZKEITCHO3UT4DGUCRC"
+    
+    let date = "1/7/2018"
+    
+    func witNetwork(toBeSentString : String){
+        
+        let params : [String : String] = ["v" : date , "q" : toBeSentString]
+        let authHeader : [String : String] = ["Authorization" : witAppToken]
+        
+        print("to be sent: " + toBeSentString)
+        
+        
+        Alamofire.request(baseURL, method: .get, parameters: params, headers: authHeader).responseJSON { (response) in
+            if response.result.isSuccess{
+                print("Success ... Got data from Wit.ai")
+                
+                let witResponse = JSON(response.result.value!)
+                
+                self.delegate?.recieveResponse(response: witResponse)
+                
+            } else {
+                print("Error: \(response.result.error!)")
+            }
+        }
+    }
+    
+}
